ff474afc783ef0bf32e0f99f4bc525e648651cab
placebo889 <hentalia2d@gmail.com>
2024-01-03 10:34:13 +0200

    fix funcs and options

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,17 +17,27 @@ export class App extends Component {
       [type]: prevState[type] + 1,
     }));
   };
+ countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+    return total === 0 ? 0 : (good / total) * 100;
+  };
 
   render() {
     const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentage = total === 0 ? 0 : (good / total) * 100;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
 
     return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
+            options={Object.keys(this.state)}
             onLeaveFeedback={this.onLeaveFeedback}
           />
         </Section>
